b0268d630305a5c1e7e1c8abdd60d545c0d1abe8
andrew.tiema <andrew.tiema@ajiraconnect.com>
2021-05-05 20:45:13 +0300

    fix/api error

--- Hymn/model/Hymn.swift
+++ Hymn/model/Hymn.swift
@@ -10,20 +10,20 @@ import Foundation
 struct Hymn: Codable {
     let id: Int
     let title: String
-    let songNumber: Int
+    let songNumber: Int?
     let chorus: String
     let song: String
     let category_id: Int
     let created_at: String
     let updated_at: String
-    let category: Category
+    var category: Category?
 }
 
 struct Category: Codable {
     let id: Int
     let name: String
-    let createdAt: String
-    let updatedAt : String
+    let createdAt: String?
+    let updatedAt : String?
     
     enum CodingKeys: String, CodingKey {
         case id
---
+++ Hymn/utils/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Hymn
+//
+//  Created by Andrew Ananda on 05/05/2021.
+//
+
+import Foundation
+
+struct K {
+    struct endpoint {
+        static let BASE_URL = "http://hymn.devstart.co.ke/api/"
+        static let songs = "songs"
+    }
+}
